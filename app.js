d5c9fd0f2b816a03cfbe1fc0dc68b34ae09f9dc1
ksdoskd <160746514+ksdoskd@users.noreply.github.com>
2024-06-05 07:34:53 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+if('serviceWorker' in navigator)
+	{
+		/*Empezamo con una promesa de registro.*/
+		navigator.serviceWorker.register('/sw.js')
+		.then((reg) => console.log("Service Worker Registrado!", reg))/*Si la promesa es exitosa, se para el parametro "reg" y se imprime el mensaje por consola.*/
+		.catch((err) => console.log("Service Worker No Registrado!", err));/*Sino, se para el parametro "err" y se imprime el mensaje por consola.*/
+	}
